48fa8f6cda321a8206d8e615c1878ee165a8e4a5
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-04-16 19:00:39 -0400

    not able to render yet

--- src/components/NoteContainer.js
+++ src/components/NoteContainer.js
@@ -2,17 +2,40 @@ import React from "react";
 import Search from "./Search";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
+import { useEffect, useState } from "react";
 
 function NoteContainer() {
-  return (
-    <>
-      <Search />
-      <div className="container">
-        <Sidebar />
-        <Content />
-      </div>
-    </>
-  );
+
+  const [notes, setNotes] = useState([])
+  const [chosen, setChosen] = useState(null)
+  const [searchString, setSearchString] = useState('')
+
+
+  useEffect( () => {
+    fetch('http://localhost:3000/notes')
+    .then(r => r.json())
+    .then(data => setNotes(data))}, [])
+
+  function handleSearched (string) {
+    setSearchString(string.toLowerCase())
+  }
+  const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(searchString.toLowerCase()))
+ 
+  function addNewNote (newNote) {
+    setNotes([...notes, newNote])
+  }
+
+  const chosenNote = notes.find((note) => note.id === parseInt(chosen))
+
+return (
+  <>
+    <Search handleSearched={handleSearched}/>
+    <div className="container">
+      <Sidebar addNewNote={addNewNote} handleChosen={setChosen} notes={filteredNotes}/>
+      <Content chosenNote={chosenNote} chosen={chosen}/>
+    </div>
+  </>
+);
 }
 
 export default NoteContainer;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,32 @@
 import React from "react";
 import NoteList from "./NoteList";
 
-function Sidebar() {
-  return (
-    <div className="master-detail-element sidebar">
-      <NoteList />
-      <button>New</button>
-    </div>
-  );
-}
+function Sidebar({notes, handleChosen, addNewNote}) {
 
+  function handleClick () {
+    const newNote = {
+       "userId": 1,
+       "title":"default",
+       "body": "placeholder"
+     }
+     fetch('http://localhost:3000/notes', {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify(newNote)
+     })
+     .then(r=> r.json())
+     .then(note => addNewNote(note))
+   }
+ 
+ 
+ 
+   return (
+     <div className="master-detail-element sidebar">
+       <NoteList notes = {notes} handleChosen={handleChosen}/>
+       <button onClick={handleClick} >New</button>
+     </div>
+   );
+ }
 export default Sidebar;
